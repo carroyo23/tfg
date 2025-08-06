d42d02b2b138b4e2f0de04f6bf710bdaeed86ee1
carroyo23 <carroyole2002@gmail.com>
2025-08-06 13:58:37 +0200

    added operador de cruce BLX-alpha al genetico

--- src/Genetico.java
+++ src/Genetico.java
@@ -28,20 +28,55 @@ class Resumen {
 	public float porcentaje_superado = -1;
 	public float tiempo_restante = -1;
 	public int monedas_conseguidas = -1;
+	
+	public Resumen(){
+		
+	}
+	
+	public Resumen(int nuevo_nivel, float nuevo_perc, float nuevo_tiempo, int nuevo_monedas){
+		niveles_superados = nuevo_nivel;
+		porcentaje_superado = nuevo_perc;
+		tiempo_restante = nuevo_tiempo;
+		monedas_conseguidas = nuevo_monedas;
+	}
+	
+	public Resumen(final Resumen otro) {
+		niveles_superados = otro.niveles_superados;
+		porcentaje_superado = otro.porcentaje_superado;
+		tiempo_restante = otro.tiempo_restante;
+		monedas_conseguidas = otro.monedas_conseguidas;
+	}
 }
 
 class Individuo {
-	public float horizontal = 0;
-	public float vertical = 0;
-	public float kill = 0;
-	public float monedas = 0;
-	Resumen resultados;
-	float fitness;
+	// la forma de este array sera la siguiente: [valor_horizontal, valor_vertical, valor_kill, valor_monedas]
+	public float[] genoma = null;
+	
+	public Resumen resultados = null;
+	public float fitness = -1;
 	
 	public final float PESO_NIVEL = 100f;
 	public final float PESO_PORCENTAJE = 70f;
 	public final float PESO_TIEMPO = 30f;
 	public final float PESO_MONEDAS = 5f;
+	
+	public Individuo() {
+		genoma = null;
+		resultados = null;
+		fitness = -1;
+	}
+	
+	public Individuo(float[] nuevo_genoma) {
+		genoma = nuevo_genoma.clone();
+		resultados = null;
+		fitness = -1;
+	}
+	
+	public Individuo(final Individuo otro) {
+		genoma = otro.genoma.clone();
+		fitness = otro.fitness;
+		resultados = new Resumen(otro.resultados);
+	}
 	
 	
 	public float getFitness() {
@@ -61,7 +96,7 @@ public class Genetico {
 	public static Random generador_random = new Random(42);
 	
 	// el operador de seleccion sera por torneo de 3 individuos
-	public static List<Individuo> op_seleccion(List<Individuo> poblacion, int tam_reemplazo) {
+	public static List<Individuo> op_seleccion(final List<Individuo> poblacion, int tam_reemplazo) {
 		List<Individuo> nuevos = new ArrayList<>();
 		
 		int num_individuos = poblacion.size();
@@ -100,7 +135,36 @@ public class Genetico {
 		return nuevos;
 	}
 	
-	
+	// operador de cruce (cruza 2 padres para generar 2 hijos) (solo paso los genomas porque el resto no lo necesito)
+	// se usara el cruce BLX-alpha
+	public static Individuo[] op_cruce(final float[] uno, final float[] otro, float alpha, final Random generador_random) {
+		Individuo[] hijos = new Individuo[2];
+		
+		float min, max, diff;
+		float[] primer_genoma = uno.clone();
+		float[] segundo_genoma = otro.clone();
+		
+		// inicializo cada hijo a un padre
+		
+		// itero sobre cada gen
+		for (int i = 0; i < primer_genoma.length; i++) {
+			
+			// calculo la diferencia, el maximo y el minimo
+			min = Math.min(primer_genoma[i], segundo_genoma[i]);
+			max = Math.max(primer_genoma[i], segundo_genoma[i]);
+			diff = max - min;
+			
+			// asigno en cada gen un valor aleatorio dentro del rango que marca el operador de cruce BLX-alpha
+			primer_genoma[i] = (float) generador_random.nextDouble(min - alpha*diff, max + alpha*diff);
+			segundo_genoma[i] = (float) generador_random.nextDouble(min - alpha*diff, max + alpha*diff);
+		}
+		
+		// relleno los hijos (añado el genoma y dejo los resultados como indeterminados hasta que se evaluen
+		hijos[0] = new Individuo(primer_genoma);
+		hijos[1] = new Individuo(segundo_genoma);
+		
+		return hijos;
+	}
 	
 	public static void printResults(MarioResult result) {
         System.out.println("****************************************************************");
